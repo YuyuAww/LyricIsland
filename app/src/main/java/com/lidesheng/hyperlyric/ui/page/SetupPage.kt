23a8946fd49fc04a70209f62a753a54cdc8b9a82
YuyuAww <YuyuAww@users.noreply.github.com>
2026-07-13 02:39:08 +0000

    style: 移除重复的Column import

--- app/src/main/java/com/lidesheng/hyperlyric/ui/page/SetupPage.kt
+++ app/src/main/java/com/lidesheng/hyperlyric/ui/page/SetupPage.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import top.yukonga.miuix.kmp.basic.BasicComponent
